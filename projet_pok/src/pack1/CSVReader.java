9afa5e4d8af862aa6e3cbd4a2997a49d5978536a
Johan Palos <jojo24.palos@gmail.com>
2023-04-14 16:31:40 +0200

    modif

--- projet_pok/src/pack1/CSVReader.java
+++ projet_pok/src/pack1/CSVReader.java
@@ -7,6 +7,46 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CSVReader {
+	
+	public static void main(String[] args) {
+        String fichierCSV = "../data/pokemon_first_gen.csv";
+        char separateur = ',';
+
+        List<Pokemon> listePokemons = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(fichierCSV))) {
+            String ligne;
+            boolean ligne1 = true;
+            while ((ligne = br.readLine()) != null) {
+            	if(ligne1) {
+            		ligne1 = false;
+            		continue;
+            	}
+                
+                String[] valeurs = ligne.split(String.valueOf(separateur));
+                
+                int id = Integer.parseInt(valeurs[0]);
+                String nom = valeurs[1];
+                String type = valeurs[2];
+                String type2 = valeurs[3];
+                double Total = Integer.parseInt(valeurs[4]);
+                Pokemon pokemon = new Pokemon(id, nom, type, type2, Total);
+               
+                listePokemons.add(pokemon);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        for (Pokemon pokemon : listePokemons) {
+            System.out.println(pokemon.toString());
+        }
+    }
+
+	
+	
+	
+/*	
 
     public static String[][] readCSV(String nomFichier, char c) throws IOException {
         List<String[]> lignes = new ArrayList<>();
@@ -19,15 +59,17 @@ public class CSVReader {
         }
         return lignes.toArray(new String[0][0]);
     }
+    
+    
 
     public static void main(String[] args) {
         try {
-            String[][] tableau = readCSV("C:\\1_MAJ\\Projet\\Java\\FAGUET-LEROUX-PALOS\\data\\pokemon_first_gen.csv", ',');
+            String[][] tableau = readCSV("../data/pokemon_first_gen.csv", ',');
 
             for (int i = 1; i < tableau.length; i++) {
                 String[] ligne = tableau[i];
 
-                Pokemon pokemon = new Pokemon(1,"","");
+                Pokemon pokemon = new Pokemon();
                 pokemon.setId(Integer.parseInt(ligne[0]));
                 pokemon.setNom(ligne[1]);
                 pokemon.setType(ligne[2]);
@@ -39,5 +81,5 @@ public class CSVReader {
             e.printStackTrace();
         }
     }
-    
+    */
 }
--- projet_pok/src/pack1/Pokemon.java
+++ projet_pok/src/pack1/Pokemon.java
@@ -5,11 +5,19 @@ public class Pokemon {
 	private int id;
     private String nom;
     private String type;
+    private String type2;
+    private double Total;
     
-    public Pokemon(int id, String nom, String type) {
+    public Pokemon(int id, String nom, String type, String type2, double Total) {
     	this.id = id;
     	this.nom = nom;
     	this.type = type;
+    	this.type2 = null;
+    	this.Total = Total;
+    }
+    
+    public Pokemon() {
+    	
     }
     
     public int getId() {
@@ -35,10 +43,28 @@ public class Pokemon {
     public void setType(String type) {
     	this.type = type;
     }
+    
+    public String getType2() {
+    	return this.type;
+    }
+    
+    public void setType2(String type) {
+    	this.type2 = null;
+    }
+    
+    public double getTotal() {
+    	return this.Total;
+    }
+    
+    public void setTotal(double Total) {
+    	this.Total = Total;
+    }
 
 
     @Override
     public String toString() {
-        return "Pokemon [id=" + id + ", nom=" + nom + ", type=" + type + "]";
+        return "Pokemon [id=" + getId() + ", nom=" + getNom() + ", type=" + getType() + ", type2=" + getType2() + 
+        		", Total=" + getTotal() + "]";
     }
+    
 }
