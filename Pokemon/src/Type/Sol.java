4cf3ecfd6469eb541da2ccae099f19b9a08f8ff4
alexxandre17 <alex.esteoulle@gmail.com>
2023-04-14 21:11:50 +0200

    types, debut attaque, debut deplacement

---
+++ Pokemon/src/Type/Sol.java
@@ -0,0 +1,98 @@
+package Type;
+
+public class Sol {
+	
+	public static final String[] Efficace= {"Acier","Roche","Feu","Electrique","Poison"};
+	public static final String[] PasEfficace= {"Plante","Insecte"};
+	public static final String[] Neutre= {"Dragon","Combat","Fee","Normal"
+			,"Psy","Spectre","Tenebres","Sol","Eau","Glace"};
+	public static final String[] Inefficace= {"Vol"};
+	
+	public static boolean contains(String typeAdv , String[] Efficacite ) {
+		for(int i=0;i<Efficacite.length;i++) {
+			if(typeAdv==Efficacite[i]) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public static double AttaqueDouble(String typeAdv1,String typeAdv2) {
+		
+		if ( contains(typeAdv1, Efficace) && contains(typeAdv2, Efficace) ) {
+			System.out.println("Hyper Efficace ");
+			return 4;
+			
+		}	// 2 efficaces
+		
+		if ( (contains(typeAdv1, Efficace) && contains(typeAdv2, Neutre))
+				||(contains(typeAdv1, Neutre) && contains(typeAdv2, Efficace)) ) {
+			System.out.println("Super Efficace ");
+			return 2;			
+		}	// 1 efficace + 1 neutre
+		
+		if ( (contains(typeAdv1, Efficace) && contains(typeAdv2, PasEfficace))
+				||(contains(typeAdv1, PasEfficace) && contains(typeAdv2, Efficace)) ) {
+			System.out.println("Neutre ");
+			return 1;			
+		}	// 1 efficace + 1 resistance
+		
+		if ( (contains(typeAdv1, Efficace) && contains(typeAdv2, Inefficace))
+				||(contains(typeAdv1, Inefficace) && contains(typeAdv2, Efficace)) ) {
+			System.out.println("Ca n'affecte pas :( ");
+			return 0;			
+		}	// 1 efficace + 1 inefficace
+		
+		if ( contains(typeAdv1, Neutre) && contains(typeAdv2, Neutre) ) {
+			System.out.println("Neutre ");
+			return 1;			
+		}	// 2 neutres
+		
+		if ( (contains(typeAdv1, Neutre) && contains(typeAdv2, PasEfficace))
+				||(contains(typeAdv1, PasEfficace) && contains(typeAdv2, Neutre)))  {
+			System.out.println("Pas très Efficace ");
+			return 0.5;			
+		} 	// 1 neutre + 1 pas efficace
+		
+		if ( (contains(typeAdv1, Neutre) && contains(typeAdv2, Inefficace))
+				||(contains(typeAdv1, Inefficace) && contains(typeAdv2, Neutre)) ) {
+			System.out.println("Ca n'affecte pas :( ");
+			return 0;			
+		}	// 1 neutre + 1 inefficace
+		
+		if ( contains(typeAdv1, PasEfficace) && contains(typeAdv2, PasEfficace) ) {
+			System.out.println("Tres peu Efficace ");
+			return 0.25;			
+		}	// 2 pas efficaces
+		
+		else  {
+			System.out.println("Ca n'affecte pas :( ");
+			return 0;			
+		}	// 1 pas efficace + 1 inefficace ou 2inefficaces(impossible)
+		
+	}
+	
+public static double AttaqueSimple(String typeAdv) {
+		
+		if (contains(typeAdv, Efficace)) {
+			System.out.println("Super Efficace ");
+			return 2;			
+		}	// efficace 
+		
+		if (contains(typeAdv, Neutre)) {
+			System.out.println("Neutre ");
+			return 1;			
+		}	// neutre
+		
+		if (contains(typeAdv, PasEfficace)) {
+			System.out.println("Pas tres efficace");
+			return 0.5;			
+		}	// pas afficace 
+		
+		else {
+			System.out.println("Ca n'affecte pas :( ");
+			return 0;			
+		}	// inefficace
+}
+
+}
