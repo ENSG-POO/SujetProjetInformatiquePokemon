82dc8cd613e74845d0db24fe7486af2ba70b0eaf
Joanna <gosse.joanna@gmail.com>
2023-04-15 18:56:39 +0200

    Fix pokemon

--- Play/src/pokemons/MainJoueur.java
+++ Play/src/pokemons/MainJoueur.java
@@ -7,9 +7,9 @@ public class MainJoueur {
 	public static void main(String[] args) {
 		ArrayList<Pokemon> lst = new ArrayList<Pokemon>();
 		
-		lst.add(new Pokemon("Arceus", 200, "Glace"));
-		lst.add(new Pokemon("Pikapika", 500, "Feu"));
-		lst.add(new Pokemon("Dialga", 180, "Eau"));
+		lst.add(new Pokemon("Arceus", "Glace", 200, 30, 50, 40));
+		lst.add(new Pokemon("Pikapika", "Electrique", 500, 56, 70, 50));
+		lst.add(new Pokemon("Dialga", "Eau", 180, 45, 70, 54));
 
 		Joueur j1 = new Joueur("Joanna", 3, lst);
 		
--- Play/src/pokemons/Pokemon.java
+++ Play/src/pokemons/Pokemon.java
@@ -1,84 +1,59 @@
 package pokemons;
 //import java.util.List;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.List;
-import java.util.ArrayList;
-
 public class Pokemon {
-	private String name;
-	private int pv;
-	private String type;
+	public String name;
+	public String type;
+	public int pv;
+	public int attack;
+	public int defense;
+	public int speed;
 	
 	//private List<Attaques> attaques;
 	
-	public Pokemon(String name, int pv, String type) { //List<Attaques> attaques
+	public Pokemon(String name, String type) {
+	this.name = name;
+	this.type = type;
+	}
+	
+	public Pokemon(String name, String type, int pv, int attack, int defense, int speed) {
 		this.name = name;
-		this.pv = pv;
 		this.type = type;
-		//this.attaques = attaques;
+		this.pv = pv;
+		this.attack = attack;
+		this.defense = defense;
+		this.speed = speed;
+	}
+	
+	//Setters
+	
+	
+	//Getters
+	public String getName() {
+		return name;
+	}
+	public String getType() {
+		return type;
+	}
+	public int getPV(){
+		return pv;
+	}
+	public int getAttack() {
+		return attack;
+	}
+	public int getDefense() {
+		return defense;
+	}
+	public int getSpeed() {
+		return speed;
 	}
 	
 	
-	public List<List<String>> pokemonAttributes() {
-		String csvFile = "C:/Users/gosse/Documents/GitHub/Correia_Gosse_Liu/Play/src/pokemons/pokemon_first_gen.csv";
-	    BufferedReader br = null;
-	    String line = "";
-	    String cvsSplitBy = ",";
-	    List<List<String>> allPoke = new ArrayList<>();
-
-
-	    try {
-
-	        br = new BufferedReader(new FileReader(csvFile));
-	        br.readLine();
-	        while ((line = br.readLine()) != null) {
-
-	            // use comma as separator
-	        	
-	            String[] pokemon_attribute = line.split(cvsSplitBy);
-	            List<String> poke = new ArrayList<>();
-	            
-	            String name = pokemon_attribute[1];
-	            String type = pokemon_attribute[2];
-	            String pv = pokemon_attribute[5];
-	            String attack = pokemon_attribute[6];
-	            String defense = pokemon_attribute[7];
-	            String speed = pokemon_attribute[8];
-	            //int speed = Integer.parseInt(pokemon_attribute[8]);
-	            
-	            poke.add(name);
-	            poke.add(type);
-	            poke.add(pv);
-	            poke.add(attack);
-	            poke.add(defense);
-	            poke.add(speed);
-	            
-	            allPoke.add(poke);
-	            
-	        }
-	    } catch (FileNotFoundException e) {
-	        e.printStackTrace();
-	    } catch (IOException e) {
-	        e.printStackTrace();
-	    } finally {
-	        if (br != null) {
-	            try {
-	                br.close();
-	            } catch (IOException e) {
-	                e.printStackTrace();
-	            }
-	        }
-	    }
-	    return allPoke;
-	}
+	
 	
 	@Override
 	public String toString() {
-		return name + ": " + pv + "PV";
+		return name + ": type " + type + " - "+ pv + " PV - Att " + attack + " - Def " + defense + " - Sp " + speed;
 	}
 
 }
