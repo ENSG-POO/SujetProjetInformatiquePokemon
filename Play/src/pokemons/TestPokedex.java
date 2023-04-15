c8855039730449d1bd86d241a2d80552b36ba133
Joanna <gosse.joanna@gmail.com>
2023-04-15 21:35:40 +0200

    Update TestPokedex.java

--- Play/src/pokemons/TestPokedex.java
+++ Play/src/pokemons/TestPokedex.java
@@ -4,8 +4,11 @@ public class TestPokedex {
 
 	public static void main(String[] args) {
 		String filename = "C:/Users/gosse/Documents/GitHub/Correia_Gosse_Liu/Play/src/pokemons/pokemon_first_gen.csv";
-		Pokedex pokedex = new Pokedex(filename);
-		System.out.println(pokedex);
+		String filecoord = "C:/Users/gosse/Documents/GitHub/Correia_Gosse_Liu/Play/src/pokemons/pokemon_coordinates.csv";
+		//Pokedex pokedex = new Pokedex(filename);
+		PokedexS pokedexS = new PokedexS(filecoord);
+		//System.out.println(pokedex);
+		System.out.println(pokedexS);
 	}
 
 }
