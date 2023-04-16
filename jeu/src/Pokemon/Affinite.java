9e4f8953a851e14d7459ac8f5677c30e8790d511
clara-cc <limy.libra@gmail.com>
2023-04-16 18:38:33 +0200

    Ajout Type et Affinite

---
+++ jeu/src/Pokemon/Affinite.java
@@ -0,0 +1,115 @@
+package Pokemon;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Affinite {
+	// attribut
+	public ArrayList<List<Double>> affinite;
+	public ArrayList<String> associationAffinite;
+	
+	// constructeur
+	public Affinite() {
+		setAffinite();
+		setAssociationAffinite();
+	}
+	
+	// getter
+	public ArrayList<List<Double>> getAffinite(){ return this.affinite;}
+	public ArrayList<String> getAssociationAffinite(){ return this.associationAffinite;}
+	
+	//setter
+	public void setAffinite() {
+			this.affinite = new ArrayList<List<Double>>();
+			List<Double> NormalValues = Arrays.asList(1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 1.0, 1.0, 1.0, 1.0);
+			List<Double> FireValues = Arrays.asList(1.0, 0.5, 2.0, 0.5, 1.0, 0.5, 1.0, 1.0, 2.0, 1.0, 1.0, 0.5, 2.0, 1.0, 1.0, 1.0, 0.5, 0.5);
+			List<Double> WaterValues = Arrays.asList(1.0, 0.5, 0.5, 2.0, 2.0, 0.5, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.5, 1.0);
+			List<Double> GrassValues = Arrays.asList(1.0, 2.0, 0.5, 0.5, 0.5, 2.0, 1.0, 2.0, 0.5, 2.0, 1.0, 2.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0);
+			List<Double> ElectricValues = Arrays.asList(1.0, 1.0, 1.0, 1.0, 0.5, 1.0, 1.0, 1.0, 2.0, 0.5, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.5, 1.0);
+			List<Double> IceValues = Arrays.asList(1.0, 2.0, 1.0, 1.0, 1.0, 0.5, 2.0, 1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 1.0, 1.0, 1.0, 2.0, 1.0);
+			List<Double> FightingValues = Arrays.asList(1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 2.0, 0.5, 0.5, 1.0, 1.0, 0.5, 1.0, 2.0);
+			List<Double> PoisonValues = Arrays.asList(1.0, 1.0, 1.0, 0.5, 1.0, 1.0, 0.5, 0.5, 2.0, 1.0, 2.0, 0.5, 1.0, 1.0, 1.0, 1.0, 1.0, 0.5);
+			List<Double> GroundValues = Arrays.asList(1.0, 1.0, 2.0, 2.0, 0.0, 2.0, 1.0, 0.5, 1.0, 1.0, 1.0, 1.0, 0.5, 1.0, 1.0, 1.0, 1.0, 1.0);
+			List<Double> FlyingValues = Arrays.asList(1.0, 1.0, 1.0, 0.5, 2.0, 2.0, 0.5, 1.0, 0.0, 1.0, 1.0, 0.5, 2.0, 1.0, 1.0, 1.0, 1.0, 1.0);
+			List<Double> PsychicValues = Arrays.asList(1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.5, 1.0, 1.0, 1.0, 0.5, 2.0, 1.0, 2.0, 1.0, 2.0, 1.0, 1.0);
+			List<Double> BugValues = Arrays.asList(1.0, 2.0, 1.0, 0.5, 1.0, 1.0, 0.5, 1.0, 0.5, 2.0, 1.0, 1.0, 2.0, 1.0, 1.0, 1.0, 1.0, 1.0);
+			List<Double> RockValues = Arrays.asList(0.5, 0.5, 2.0, 2.0, 1.0, 1.0, 2.0, 0.5, 2.0, 0.5, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 1.0);
+			List<Double> GhostValues = Arrays.asList(0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.5, 1.0, 1.0, 1.0, 0.5, 1.0, 2.0, 1.0, 2.0, 1.0, 1.0);
+			List<Double> DragonValues = Arrays.asList(1.0, 0.5, 0.5, 0.5, 0.5, 2.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 1.0, 1.0, 2.0);
+			List<Double> DarkValues = Arrays.asList(1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 1.0, 1.0, 1.0, 0.0, 2.0, 1.0, 0.5, 1.0, 0.5, 1.0, 2.0);
+			List<Double> SteelValues = Arrays.asList(0.5, 2.0, 1.0, 0.5, 1.0, 0.5, 2.0, 0.0, 2.0, 0.5, 0.5, 0.5, 0.5, 1.0, 0.5, 1.0, 0.5, 0.5);
+			List<Double> FairyValues = Arrays.asList(1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.5, 2.0, 1.0, 1.0, 1.0, 0.5, 1.0, 1.0, 0.0, 0.5, 2.0, 1.0);
+			
+			this.affinite.add(NormalValues);
+			this.affinite.add(FireValues);
+			this.affinite.add(WaterValues);
+			this.affinite.add(GrassValues);
+			this.affinite.add(ElectricValues);
+			this.affinite.add(IceValues);
+			this.affinite.add(FightingValues);
+			this.affinite.add(PoisonValues);
+			this.affinite.add(GroundValues);
+			this.affinite.add(FlyingValues);
+			this.affinite.add(PsychicValues);
+			this.affinite.add(BugValues);
+			this.affinite.add(RockValues);
+			this.affinite.add(GhostValues);
+			this.affinite.add(DragonValues);
+			this.affinite.add(DarkValues);
+			this.affinite.add(SteelValues);
+			this.affinite.add(FairyValues);
+	}
+	
+	public void setAssociationAffinite() {
+			this.associationAffinite = new ArrayList<String>();
+			// met dans l'ordre les types
+			this.associationAffinite.add("Normal");
+			this.associationAffinite.add("Fire");
+			this.associationAffinite.add("Water");
+			this.associationAffinite.add("Grass");
+			this.associationAffinite.add("Electric");
+			this.associationAffinite.add("Ice");
+			this.associationAffinite.add("Fighting");
+			this.associationAffinite.add("Poison");
+			this.associationAffinite.add("Ground");
+			this.associationAffinite.add("Flying");
+			this.associationAffinite.add("Psychic");
+			this.associationAffinite.add("Bug");
+			this.associationAffinite.add("Rock");
+			this.associationAffinite.add("Ghost");
+			this.associationAffinite.add("Dragon");
+			this.associationAffinite.add("Dark");
+			this.associationAffinite.add("Steel");
+			this.associationAffinite.add("Fairy");
+	}
+	
+	// methode
+	
+	// donne un type et sort son index
+	public int association(String nomType) {
+		for (int i= 0; i<this.associationAffinite.size(); i++) {
+			if (this.associationAffinite.get(i).equals(nomType)) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+	// donne un index et sort un type
+	public String valeurIndex(int idx) {
+		return this.associationAffinite.get(idx);
+	}
+
+	@Override
+	public String toString() {
+		for (int i=0; i<this.affinite.size(); i++) {
+			System.out.println(this.associationAffinite.get(i));
+			for (int j=0; j<this.affinite.get(i).size(); j++) {
+				System.out.println(this.affinite.get(j).get(i));
+			}
+		}
+		return "";
+	}
+}
+
---
+++ jeu/src/Pokemon/Type.java
@@ -0,0 +1,62 @@
+package Pokemon;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Type {
+	// attributs 
+	public int indexAssociationAffinite;
+	public String nomT;
+	public Affinite aff;
+	public List<String> faiblesse;
+	public List<String> resistance;
+	public List<String> invulnerable;
+	
+	// constructeur
+	public Type(String nomType) {
+		this.nomT = nomType;
+		this.aff = new Affinite();
+		this.indexAssociationAffinite = aff.association(nomType);
+		setConfiguration();
+		
+	}
+	
+	// getter
+	public List<String> getFaiblesse(){return this.faiblesse;}
+	public List<String> getResistance(){return this.resistance;}
+	public List<String> getInvulnerable(){return this.invulnerable;}
+	public String getNom() {return this.nomT;}
+	public String getAffinite() {return this.aff.toString();}
+	
+
+	// setter 
+	public void setConfiguration() {
+		this.faiblesse = new ArrayList<String>();
+		this.resistance = new ArrayList<String>();
+		this.invulnerable = new ArrayList<String>();
+		// parcours le tableau d'affinite
+		for (int i=0; i<this.aff.getAffinite().size(); i++) {
+			// charge la liste d'invulnerable
+			if (this.aff.getAffinite().get(this.indexAssociationAffinite).get(i) == 0.0) {
+				this.invulnerable.add(this.aff.valeurIndex(i));
+			}
+			// charge la liste de resistance
+			if (this.aff.getAffinite().get(this.indexAssociationAffinite).get(i) == 0.5) {
+				this.resistance.add(this.aff.valeurIndex(i));
+			}
+			// charge la liste de faiblesse
+			if (this.aff.getAffinite().get(this.indexAssociationAffinite).get(i)== 2.0) {
+				this.faiblesse.add(this.aff.valeurIndex(i));
+			}
+		}
+	}
+	
+	// ecriture
+	@Override
+	public String toString() {
+		return ("Type du pokemon : "+ this.nomT + "\nSes faiblesses : "+ this.faiblesse + " \tSes resistances : "+ this.resistance + "\tSes invulnerabilite : "+ this.invulnerable);
+		
+	}
+	
+
+}
