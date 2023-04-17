bb92e00e8f683f69c87ea9a616d9f0cc1c166237
clara-cc <limy.libra@gmail.com>
2023-04-17 15:41:16 +0200

    classe attaque

--- jeu/src/pokemons/Attaque.java
+++ jeu/src/pokemons/Attaque.java
@@ -1,7 +1,31 @@
 package pokemons;
 
-public interface Attaque {	
-	// methode 
-	public void attaque(Pokemon p, String nom_a);
-
+public class Attaque {
+	// attributs 
+	public String nomA;
+	public Type type;
+	
+	// constructeurs
+	public Attaque(String nomA, Type type) {
+		this.nomA = nomA;
+		this.type = type;
+	}
+	
+	// getter
+	public String getNomAttaque() { return this.nomA;}
+	public String getTypeAttaque() { return this.type.nomT;}
+	
+	// methode
+	public double coefMulti(Type typeAtt, Type typeDef) {
+		int indexAtt = typeAtt.indexAssociationAffinite;
+		int indexDef = typeDef.indexAssociationAffinite;
+		double valMultiplicateur = typeAtt.aff.affinite.get(indexDef).get(indexAtt);
+		//System.out.println("Attak index def - index att = "+ valMultiplicateur);
+		return valMultiplicateur;
+	}
+	
+	@Override
+	public String toString() {
+		return ("Attaque " + this.nomA + " de type "+ getTypeAttaque());
+	}
 }
