81e4b59310e0ce293cc48aab8cd60604d0f98e11
bl-ensg <130739080+bl-ensg@users.noreply.github.com>
2023-04-18 13:59:24 +0200

    Add files via upload

--- jeu/src/jeu/eu/ensg/Type.java
+++ jeu/src/jeu/eu/ensg/Type.java
@@ -4,11 +4,6 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
 
-/**
- * 
- * @author Test
- *
- */
 public class Type {
 	
 	private String nom;
@@ -20,36 +15,30 @@ public class Type {
 	
 	public int NumType() {
 		HashMap<String,Double> NumType = new HashMap<String,Double>();
-		NumType.put("Acier",0.0);
-		NumType.put("Combat",1.0);
+		NumType.put("Steel",0.0);
+		NumType.put("Fighting",1.0);
 		NumType.put("Dragon",2.0);
-		NumType.put("Eau",3.0);
-		NumType.put("Feu",4.0);
-		NumType.put("Electrik",5.0);
-		NumType.put("Fee",6.0);
-		NumType.put("Glace",7.0);
-		NumType.put("Insecte",8.0);
+		NumType.put("Water",3.0);
+		NumType.put("Fire",4.0);
+		NumType.put("Electric",5.0);
+		NumType.put("Fairy",6.0);
+		NumType.put("Ice",7.0);
+		NumType.put("Bug",8.0);
 		NumType.put("Normal",9.0);
-		NumType.put("Plante",10.0);
+		NumType.put("Grass",10.0);
 		NumType.put("Poison",11.0);
-		NumType.put("Psy",12.0);
-		NumType.put("Roche",13.0);
-		NumType.put("Sol",14.0);
-		NumType.put("Spectre",15.0);
-		NumType.put("tenebre",16.0);
-		NumType.put("Vol",17.0);
+		NumType.put("Psychic",12.0);
+		NumType.put("Rock",13.0);
+		NumType.put("Ground",14.0);
+		NumType.put("Ghost",15.0);
+		NumType.put("Dark",16.0);
+		NumType.put("Flying",17.0);
 		int nt = NumType.get(this.nom).intValue();
 		return nt;
 	}
 
 
 	public String getdata(Type b) {
-		
-		String[][] data = CSVReader.readCsvFile("./data/tableau-type.csv", ","); // ouverture du tableau des types.csv en tableau
-		return data[this.NumType()][b.NumType()]; // retourne le coefficient multiplicateur entre deux types
-		
-		/*
-		
         String csvFile = "C:/Users/Formation/Desktop/projet/Jmammou-Labbe-Bontoux/data/tableau-type.csv"; // "import" du fichier
         String line = "";
         String csvSplitBy = ","; // séparateur dans le csv qui permettra de "dispatcher" les valeurs dans le tableau crée
@@ -84,6 +73,5 @@ public class Type {
             e.printStackTrace();
         }
         return data[this.NumType()][b.NumType()];
-        */
 	}
 }
