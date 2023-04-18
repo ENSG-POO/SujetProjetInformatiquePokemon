773c441f954a39a5548355a3546e2aafcad4b946
Mateus <tom.23.mateus@gmail.com>
2023-04-18 09:58:59 +0200

    attaque

--- jeu/src/jeu/eu/ensg/attaque.java
+++ jeu/src/jeu/eu/ensg/attaque.java
@@ -2,48 +2,70 @@ package jeu.eu.ensg;
 
 public class attaque {
 	private int id;
-	private type type;
 	private String name;
-	private int Degat;
+	private type type;
+private String classe;
+private int puissance;
+private int precision ;
 
-	public int getId() {
-		return id;
-	}
+public int getId() {
+	return id;
+}
 
-	public void setId(int id) {
-		this.id = id;
-	}
+public void setId(int id) {
+	this.id = id;
+}
 
-	public type getType() {
-		return type;
-	}
+public String getName() {
+	return name;
+}
 
-	public void setType(type type) {
-		this.type = type;
-	}
+public void setName(String name) {
+	this.name = name;
+}
 
-	public String getName() {
-		return name;
-	}
+public type getType() {
+	return type;
+}
 
-	public void setName(String name) {
-		this.name = name;
-	}
+public void setType(type type) {
+	this.type = type;
+}
 
-	public int getDegat() {
-		return Degat;
-	}
+public String getClasse() {
+	return classe;
+}
 
-	public void setDegat(int degat) {
-		Degat = degat;
-	}
+public void setClasse(String classe) {
+	this.classe = classe;
+}
 
-	public attaque(int id, type type, String name, int degat) {
-		super();
-		this.id = id;
-		this.type = type;
-		this.name = name;
-		this.Degat = degat;
-	}
+public int getPuissance() {
+	return puissance;
+}
+
+public void setPuissance(int puissance) {
+	this.puissance = puissance;
+}
+
+public int getPrecision() {
+	return precision;
+}
+
+public void setPrecision(int precision) {
+	this.precision = precision;
+}
+
+public attaque(int id, String name, jeu.eu.ensg.type type, String classe, int puissance, int precision) {
+	super();
+	this.id = id;
+	this.name = name;
+	this.type = type;
+	this.classe = classe;
+	this.puissance = puissance;
+	this.precision = precision;
+}
+
+	
 
 }
