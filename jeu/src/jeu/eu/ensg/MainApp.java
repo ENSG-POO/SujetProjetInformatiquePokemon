8bde54a3b1fd364dcda6179cf0fb772902ffa673
bl-ensg <130739080+bl-ensg@users.noreply.github.com>
2023-04-19 10:18:20 +0200

    Add files via upload

--- jeu/src/jeu/eu/ensg/MainApp.java
+++ jeu/src/jeu/eu/ensg/MainApp.java
@@ -19,6 +19,8 @@ import jeu.eu.ensg.gui.ListePok;
 import jeu.eu.ensg.gui.BarreDeVie;
 import jeu.eu.ensg.gui.BoutonCombat;
 import jeu.eu.ensg.gui.Carte;
+import jeu.eu.ensg.Combat;
+import jeu.eu.ensg.Pokemon;
 
 
 public class MainApp {
@@ -86,6 +88,17 @@ public class MainApp {
 		      listePokemonSauvages.add(retardo);
 		      listePokemonSauvages.add(r);
 		      
+		      ArrayList<Pokemon> pok = new ArrayList<Pokemon>();
+			  Pokemon a = new Pokemon("ratton",s,c,Feu);
+			  Pokemon b = new Pokemon("xaxa",s,c,Feu);
+			  pok.add(a);
+			  pok.add(b);
+			  Trainer t = new Trainer(c,pok);
+			  List<String> nom_pok = new ArrayList<String>();
+			    for (int i=0;i<pok.size();i++) {
+			    	nom_pok.add(pok.get(i).getNom());
+			    }
+		      
 		      List<Pokemon> Listeproxi = C.listePokemonsProximite(listePokemonSauvages, 100);
 		      List<String> nom = new ArrayList<String>();
 		      for (int i=0;i<Listeproxi.size();i++) {
@@ -95,39 +108,37 @@ public class MainApp {
 		      JComboBox<String> cb = new JComboBox<String>(new Vector<String>(nom));
 		      JFrame selec = new JFrame();
 		      selec.add(cb);
+
 		      selec.setSize(new Dimension(200,200));
+
 		      selec.pack();
 		      selec.setVisible(true);
+		      selec.setTitle("Choisir Pokemon enemi");
 		      
 		      cb.addActionListener(new ActionListener() {
 		    	  public void actionPerformed(ActionEvent e) {
 		    		  String selectedOption = (String) cb.getSelectedItem();
 		    	      System.out.println("Selected option: " + selectedOption);
 		    	      selec.dispose();
-		    	      JFrame fen = new JFrame();
-				        fen.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-						fen.setSize(1200, 800);
-						
+		    	      
+		    	      JFrame selec2 = new JFrame();
+		    	      selec2.setSize(2000,2000);
+				      selec2.setVisible(true);
+		    	      selec2.setTitle("Choisir Pokemon dresseur");
+		    	      JComboBox<String> cb2 = new JComboBox<String>(new Vector<String>(nom_pok));
+		    	      selec2.add(cb2);
+				      selec2.pack();
+		    	      cb2.addActionListener(new ActionListener() {
+		    	    	  public void actionPerformed(ActionEvent f) {
+				    	      String selectedOption2 = (String) cb2.getSelectedItem();
+				    	      selec2.dispose();
+				    	      
+				    	      Pokemon pok_enemi = Listeproxi.get(Pokemon.FindbyName(Listeproxi, selectedOption));
+				    	      Pokemon pok_allie = Listeproxi.get(Pokemon.FindbyName(pok, selectedOption2));
+				    	      //Combat.jouer(pok_allie,pok_enemi,pok);
+		    	    	  }
+		    	      });
 
-						// just a JPanel extension, add to any swing/awt container
-						final CartePanel mapPanel = new CartePanel(); 
-						fen.add(BorderLayout.EAST, mapPanel);
-
-
-						fen.setLayout(new BorderLayout());
-						
-						fen.add(BorderLayout.CENTER, new ImageCombat());
-
-						
-						fen.add(BorderLayout.EAST, new ListePok());
-
-						fen.add(BorderLayout.SOUTH, new BoutonCombat(fen));
-						
-						fen.setLocationRelativeTo(null);
-						fen.setResizable(false);
-						fen.setTitle("fenetre de combat");
-						
-						fen.setVisible(true); 
 		    	  }
 		      });
 		 }});
